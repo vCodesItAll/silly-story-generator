598e56f13565450b823d272bb442653a951cdf9c
Vanessa Dickey <vcdickey@gmail.com>
2023-09-07 22:30:37 +0000

    added event listener and start of the function

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ function randomValueFromArray(array){
     const random = Math.floor(Math.random()*array.length);
     return array[random];
 }
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+    if(customName.value !== '') {
+        const name = customName.value;
+
+    }
+
+    if(document.getElementById('uk').checked) {
+        const weight = Math.round(300);
+        const temperature = Math.round(94);
+
+    }
+
+    story.textContent = ;
+    story.style.visibility = 'visible';
+    
+}
