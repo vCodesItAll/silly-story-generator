2e93b25b268e2ca4c38fc01be54ddd17fd96f2a8
Vanessa Dickey <vcdickey@gmail.com>
2023-09-07 22:37:25 +0000

    declared xItem, yItem, zItem

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ const randomize = document.getElementById('.randomize');
 const story = document.querySelector('.story');
 
 var storyText = "It was 94 fahrenheit outside so :insertx: went for a walk.  When they got to :inserty: they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised - :insertx: weighs 300 pounds, and it was a hot day."
-insertX = ["Batty", "Vanessa", "Hamtaro"];
+insertX = ["Batty", "Vanessa", "Shrek"];
 insertY = ["Waffle House", "Lego Land", "Atlantis"];
 insertZ = ["spontaneously combusted", "got eaten by a yeti", "turned into a slug and crawled away"];
+
+var newStory = storyText;
+
+var xItem = randomValueFromArray(insertX);
+var yItem = randomValueFromArray(insertY);
+var zItem = randomValueFromArray(insertZ);
 
 
 function randomValueFromArray(array){
@@ -30,5 +36,5 @@ function result() {
 
     story.textContent = ;
     story.style.visibility = 'visible';
-    
+
 }
