6207f363e4bbaaba327185506b230d0785bd0052
Vanessa Dickey <vcdickey@gmail.com>
2023-09-08 15:18:00 +0000

    fixed querySelectors. program runs now

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// fetches input from the form to gather name
+// references input from the form to gather name
 var customName = document.getElementById('customname');
-// registers the button click to generate story
-var randomize = document.getElementById('.randomize');
-// to populate the paragraph element with the text story
+// references the button by searching the document for the css class attribute randomize
+var randomize = document.querySelector('.randomize');
+// references the paragraph that will populate story by searching the doc for the css class story
 var story = document.querySelector('.story');
 
 var storyText = "It was 94 fahrenheit outside so :insertx: went for a walk.  When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised - :insertx: weighs 300 pounds, and it was a hot day.";
@@ -16,12 +16,13 @@ var xItem = randomValueFromArray(insertX);
 var yItem = randomValueFromArray(insertY);
 var zItem = randomValueFromArray(insertZ);
 
-newStory = newStory.replaceAll(":insertx:", xItem);
-newStory = newStory.replaceAll(":inserty:", yItem);
-newStory = newStory.replaceAll(":insertz:", zItem);
+newStory = newStory.replace(":insertx:", xItem);
+newStory = newStory.replace(":inserty:", yItem);
+newStory = newStory.replace(":insertz:", zItem);
+newStory = newStory.replace(":insertx:", xItem);
 
 function randomValueFromArray(array){
-    const random = Math.floor(Math.random()*array.length);
+    const random = Math.floor(Math.random() * array.length);
     return array[random];
 }
 
@@ -35,8 +36,9 @@ function result() {
     }
 
     if(document.getElementById('uk').checked) {
-        var weight = Math.round(300*0.07) + "stone";
-        var temperature = Math.round(94-32)*(5/9) + "centigrade";
+        var weight = Math.round(300*0.07) + " stone";
+        // having to put additional parentheses because round only applied to the subtraction
+        var temperature = Math.round((94-32)*(5/9)) + " centigrade";
         newStory = newStory.replace("300 pounds", weight);
         newStory = newStory.replace("94 fahrenheit", temperature);
 
