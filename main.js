35a8f445e82353ae68454504c040c2f3e2d0d909
Vanessa Dickey <vcdickey@gmail.com>
2023-09-08 14:20:48 +0000

    added missing punctuation

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var randomize = document.getElementById('.randomize');
 // to populate the paragraph element with the text story
 var story = document.querySelector('.story');
 
-var storyText = "It was 94 fahrenheit outside so :insertx: went for a walk.  When they got to :inserty: they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised - :insertx: weighs 300 pounds, and it was a hot day."
+var storyText = "It was 94 fahrenheit outside so :insertx: went for a walk.  When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised - :insertx: weighs 300 pounds, and it was a hot day.";
 var insertX = ["Batty", "Vanessa", "Shrek"];
 var insertY = ["Waffle House", "Lego Land", "Atlantis"];
 var insertZ = ["spontaneously combusted", "got eaten by a yeti", "turned into a slug and crawled away"];
